e445b407f51d87e5a50d4f899825a6fe8bf8528b
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-08-21 23:47:28 +0300

    project settings

---
+++ src/main/java/book/store/bookstore/BookStoreApplication.java
@@ -0,0 +1,13 @@
+package book.store.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/book/store/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package book.store.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
